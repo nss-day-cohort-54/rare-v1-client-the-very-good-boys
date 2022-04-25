4cce40f639d2ee159a0f997bd81dc32bc7cbf1c6
Christian Haggerty <captivationstation@gmail.com>
2022-04-25 11:47:23 -0500

    merge conflicts

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,41 +3,35 @@ import { Route } from "react-router-dom"
 import { CategoryList } from "../Categories/CategoriesList"
 import { TagsList } from "./tags/TagsList"
 import { UserList } from "../Users/userList"
-<<<<<<< HEAD
 import { UserDetails } from "../Users/userDetails"
 
-export const ApplicationViews = ( { tags, users } ) => {
-=======
-import { PostList } from "./posts/PostList"
-import { PostForm } from "./posts/PostForm.js"
-import { PostDetails } from "./posts/PostDetail.js"
-export const ApplicationViews = ( { tags } ) => {
->>>>>>> main
-  return (
-    <>
+export const ApplicationViews = ({ tags, users }) => {
+  import { PostList } from "./posts/PostList"
+  import { PostForm } from "./posts/PostForm.js"
+  import { PostDetails } from "./posts/PostDetail.js"
+  export const ApplicationViews = ({ tags }) => {
+    return (
+     <>
 
-   
 
-    <Route exact path="/categories">
-        <CategoryList />
-    </Route>
-      
-      <Route exact path="/tags">
-        <TagsList tags={tags} />
-      </Route>
-<<<<<<< HEAD
 
-      <Route exact path="/userManagement">
-        <UserList users={users} />
-      </Route>
+        <Route exact path="/categories">
+          <CategoryList />
+        </Route>
 
-      <Route exact path="/users/:userId(\d+)">
-        <UserDetails />
-=======
-      <Route exact path="/components/posts">
-        <PostList />
->>>>>>> main
-      </Route>
-    </>
-  )
+        <Route exact path="/tags">
+          <TagsList tags={tags} />
+        </Route>
+
+        <Route exact path="/userManagement">
+          <UserList users={users} />
+        </Route>
+
+        <Route exact path="/users/:userId(\d+)">
+          <UserDetails />
+          <Route exact path="/components/posts">
+            <PostList />
+          </Route>
+        </>
+        )
 }
